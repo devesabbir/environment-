afd3dd92556e0f82a2b4cf6d4f75b8304beae08e
devssabbir <heartedsabbirdhrubo@gmail.com>
2022-04-03 12:22:22 +0600

    environment

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const htmlwebpack = require('html-webpack-plugin')
+
+module.exports = {
+        mode : 'development',
+        entry : path.resolve(__dirname , './src/index.js'),
+        output :{
+            path : path.resolve(__dirname , 'bundle'),
+            filename : 'bundle.js'
+        },
+        devServer : {
+             static : {
+                 directory : path.resolve(__dirname , 'bundle')
+             },
+             port : 3000,
+             open : true,
+             hot : true,
+        },
+        module : {
+            rules : [
+                {
+                    test :  /\.js$/,
+                    exclude : /(node_modules)/,
+                    use :{
+                        loader : 'babel-loader',
+                        options :  {
+                            presets : ['@babel/preset-env']
+                        }
+                    }
+                },
+                {
+                    test :  /\.(css|scss)$/,
+                    use : ['style-loader','css-loader','sass-loader']
+                }
+            ]
+        },
+        
+        plugins : [
+            new htmlwebpack({
+                title : 'Html Template',
+                filename : 'index.html',
+                template : path.resolve(__dirname , './public/index.html')
+            })
+        ]
+
+}
